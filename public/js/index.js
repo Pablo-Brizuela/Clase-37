226beba5016579e02cf07989803b48f5aa8c8c6b
unknown <pablo_2002_cab_@hotmail.com>
2022-10-18 23:28:58 -0300

    Terminar el desafio

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+let elMain = document.querySelector("main")
+let elh2 = document.querySelector("h2")
+let ela = document.querySelector("a")
+let lap = document.querySelector("p")
+
+let nombre = prompt("Ingrese su Nombre")
+
+if(!nombre){
+    elh2.innerText("Invitado")
+}
+    else {
+        elh2.innerText("nombre")
+    }
+
+elh2.style.textTransform = uppercase
+
+ela.style.color = #E51B3E

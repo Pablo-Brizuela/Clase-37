0d5472139be00448969b27c93680b33a8e75bd3a
Pablo-Brizuela <pablo_2002_cab_@hotmail.com>
2022-10-19 14:49:27 -0300

    Agregando la clase

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,14 @@ let elMain = document.querySelector("main")
 let elh2 = document.querySelector("h2")
 let ela = document.querySelector("a")
 let lap = document.querySelector("p")
+let elBody = document.querySelector("body")
+let nombre = prompt("Ingrese su Nombre")
 
-let nombre = window.prompt("Ingrese su Nombre")
+elMain.style.display = "block"
+
+if (confirm("¿Desea colocar un fondo de pantalla?")) {
+    elBody.classList.add("fondo")
+  }
 
 if(!nombre){
     elh2.innerText("Invitado")
@@ -12,6 +18,12 @@ if(!nombre){
         elh2.innerText("nombre")
     }
 
-elh2.style.textTransform = uppercase
+elh2.style.textTransform = "uppercase"
 
 ela.style.color = "#E51B3E"
+
+
+if (confirm("¿Desea colocar un fondo de pantalla?")) {
+    elBody.classList.add("fondo")
+  }
+

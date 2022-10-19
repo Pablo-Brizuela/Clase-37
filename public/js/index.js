820cbb99f7215f6b21a5a4c2a58b8084e43d8102
Pablo-Brizuela <pablo_2002_cab_@hotmail.com>
2022-10-19 16:17:19 -0300

    Falta que se vea el nombre que uno pone y el for

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ let elh2 = document.querySelector("h2")
 let ela = document.querySelector("a")
 let lap = document.querySelector("p")
 let elBody = document.querySelector("body")
+
 let nombre = prompt("Ingrese su Nombre")
 
 elMain.style.display = "block"
@@ -12,10 +13,10 @@ if (confirm("¿Desea colocar un fondo de pantalla?")) {
   }
 
 if(!nombre){
-    elh2.innerText("Invitado")
+    elh2.innerText += " Invitado"
 }
     else {
-        elh2.innerText("nombre")
+        elh2.innerHTML += "Nombre"
     }
 
 elh2.style.textTransform = "uppercase"
@@ -23,7 +24,10 @@ elh2.style.textTransform = "uppercase"
 ela.style.color = "#E51B3E"
 
 
-if (confirm("¿Desea colocar un fondo de pantalla?")) {
-    elBody.classList.add("fondo")
-  }
 
+  for (let i = 0; i > lap.length; i++) {
+    if (lap[i] %2==0){
+       lap[i].classList.add("descatadoPar")       
+    } 
+        else lap[i].classList.add("destacadoImpar")
+    }
